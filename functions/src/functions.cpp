b1a842230561077bb96528c1fbdf9ad192f88e9c
Eric <ericso0303@gmail.com>
2016-07-19 01:54:02 -0700

    functions and classes in cpp

--- functions/src/functions.cpp
+++ functions/src/functions.cpp
@@ -9,7 +9,40 @@
 #include <iostream>
 using namespace std;
 
+
+//void means no return type
+void showMenu() {
+	cout << "1. Search" << endl;
+	cout << "2. View Record" << endl;
+	cout << "3. Quit" << endl;
+}
+
+int getInput() {
+	int input;
+	cin >> input;
+
+	return input;
+}
+void processSelection(int option){
+	switch(option) {
+		case 1:
+			cout << "Searching..." << endl;
+			break;
+		case 2:
+			cout << "Viewing..." << endl;
+			break;
+		case 3:
+			cout << "Quitting..." << endl;
+			break;
+		default:
+			cout << "Please select an item from the menu" << endl;
+		}
+}
 int main() {
+	int selection;
+	showMenu();
+	selection = getInput();
+	processSelection(selection);
 
 	return 0;
 }
